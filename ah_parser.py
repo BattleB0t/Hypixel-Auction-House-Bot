32d1da0b7e74adb9a0790fa901f99e9f1257fa42
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-08-19 22:19:24 +0100

    This doesn't even work lol
    Currently just putting this here as a place holder lol, it will be changed in the near future!

--- ah_parser.py
+++ ah_parser.py
@@ -1,1 +1,20 @@
+class parse:
+    def __init__(self, ah_data_obj):
+        self.ah_data_obj = ah_data_obj
+        self.ah_page_quantity = ah_data_obj.number_of_pages
 
+        self.item_set = {}
+
+    #This may take some time-
+    def parse(self):
+        for page in range(self.ah_page_quantity):
+            current_page_data = self.ah_data_obj.data[f"page{page}"]
+
+            if current_page_data["success"] == True:
+                
+                for item in current_page_data["auctions"]:
+
+                    pass
+
+            else: #If this is called it means that the page you want to parse wasnt installed successsfully.
+                continue
