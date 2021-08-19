b583ed71974c54793dc1b9e79eea6860c348ca5f
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-08-19 22:48:56 +0100

    Added functions "search_by_name"
    This still hasn't been tested, so it probably doesn't work lol.

--- ah_parser.py
+++ ah_parser.py
@@ -2,11 +2,12 @@ class parse:
     def __init__(self, ah_data_obj):
         self.ah_data_obj = ah_data_obj
         self.ah_page_quantity = ah_data_obj.number_of_pages
-
+        self.sorted_by_type = False
+        
         self.item_set = {}
 
     #This may take some time-
-    def parse(self):
+    def sort_into_type(self):
         for page in range(self.ah_page_quantity):
             current_page_data = self.ah_data_obj.data[f"page{page}"]
 
@@ -14,7 +15,25 @@ class parse:
                 
                 for item in current_page_data["auctions"]:
 
-                    pass
+                    #Creates a list of this type of item in the set of data.
+                    if item["item_name"] not in self.item_set:
+
+                        self.item_set[item["item_name"]] = [item]
+
+                    #Appends to a previously created list.
+                    else:
+                        self.item_set[item["item_name"]].append(item)
 
             else: #If this is called it means that the page you want to parse wasnt installed successsfully.
                 continue
+
+            self.sorted_by_type = True
+
+    def search_by_name(self, item_name): ##The item_name needs to be **PRECISE** like to the word.
+        if self.sorted_by_type == True:
+            try:
+                return self.item_set[item_name]
+            except:
+                return "item not found"
+        else:
+            print("You have to sort by type before running this function.")
