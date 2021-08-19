fda995e4439f1e23ff7000db56ebdb315f48a7f3
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-08-19 22:17:06 +0100

    Testing Features
    This is my current progress on this project, currently its not in a functional state however I am using this code to bug fix/test the code.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import ah_api
+from pprint import pprint
 
+api_key=""
+
+ah_instance = ah_api.auction_data({"APIKEY":api_key})
+ah_instance.load_all_data()
+
+
+while True:
+    try:
+        pprint(ah_instance.data[input("Name The Page You Want To View: ")])
+    except:
+        print("Page Not Found")
