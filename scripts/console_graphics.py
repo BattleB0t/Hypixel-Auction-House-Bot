36b3ae353d38708c5f1d7206969e5b6c0966559b
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-08-21 04:52:20 +0100

    This is my attempt at using the rich import for console graphics.
    After 2 hours of work trying to get the tables function to work with list data, I gave up and just wrote some shabby code that can display Markdown as graphics aren't the main focus of this program.

---
+++ scripts/console_graphics.py
@@ -0,0 +1,32 @@
+from rich import markdown
+from rich.console import Console
+from rich.markdown import Markdown
+
+import os
+
+console = Console()
+
+def clear():
+    # for mac and linux(here, os.name is 'posix')
+    if os.name == 'posix':
+        os.system('clear')
+    else:
+      # for windows platfrom
+        os.system('cls')
+
+def md_print(MarkdownString):
+    console.print(Markdown(MarkdownString))
+
+def format_markdown(header, sub_headers, list_items): #List items are 2d arrays with each incriment in the y-axis being another sub_header.
+    clear()
+
+    md_print(f"""# {header}""")
+
+    sub_header_incrimentor = 0
+    for sub_header in sub_headers:
+        md_print(f"""## {sub_header}""")
+
+        item_incrimentor = 0
+        for item in list_items[sub_header_incrimentor]:
+            item_incrimentor += 1
+            md_print(f"""{item_incrimentor}. {item}""")
