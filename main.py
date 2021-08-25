fcd5a7fd5757d24a301def4bf1e6c0634dad774f
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-08-25 09:53:21 +0100

    No functionality added
    main.py is still currently being used for testing purposes, that being said the code that is on screen does allow you to call the api and save all api data to an external file.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-#There is no controling code for this program yet as it is still being developed.
-#Currently the code present is being used by me for testing.
+##This is the main script.
+import scripts.functions
+import scripts.console_graphics
 
 def main():
-    #This is only here to allow me to surf through the pages of data (because there is so much and for testing) so ignore it lol
+    #Loading all page data
     page_data = scripts.functions.request_all_pages()
-    while True:
-        try:
-            print(page_data[input("Enter The Page You Would Like To View: ")])
-        except:
-            print("ERROR")
+    
+    #Saving all page data to a json file
+    scripts.functions.save_as_json(page_data, "data.json")
 
 if __name__ == "__main__":
     main()
