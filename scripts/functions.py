5d10bc6e2d66d0e23177e42553b945543f4ff5cc
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-08-25 09:51:39 +0100

    Added data saving/reading functionality
    This is to help reduce the number of api calls i am making per min, and for possible caching in the future.

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,7 +1,35 @@
 #This is the random scripts that are used by the program.
+
+import json
+
 import scripts.api_functions
 import scripts.api
 
 def request_all_pages():
     urls = scripts.api_functions.compile_all_urls()
     return scripts.api.many_requests(urls) #This contains a set, which contains all pages of the auction house
+
+
+""" #THIS IS STILL BEING DEVELOPED#
+def parse_all_page_data(all_page_data):
+
+
+    for key in all_page_data:
+        current_page = all_page_data[key]
+
+        for auction in current_page["auctions"]:
+
+            current_auction = current_page["auctions"][auction]
+"""
+
+
+def save_as_json(data, filepath, opentype="w"):
+    json_object = json.dumps(data)
+
+    with open(filepath, opentype) as file:
+        file.write(json_object)
+
+def read_from_json(filepath):
+    with open(filepath, 'r') as openfile:
+        json_object = json.load(openfile)
+    return json_object
