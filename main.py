5bc5835ca10820c139a4df949366825c106680e9
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-08-28 04:12:25 +0100

    Still being used for testing

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 ##This is the main script.
 import scripts.functions
-import scripts.console_graphics
 
 def main():
     #Loading all page data
-    page_data = scripts.functions.request_all_pages()
+    
+    #page_data = scripts.functions.request_all_pages()
     
     #Saving all page data to a json file
-    scripts.functions.save_as_json(page_data, "data.json")
+    
+    #scripts.functions.save_as_json(page_data, "data.json")
+
+
+    page_data = scripts.functions.read_from_json("data.json")
+
+    scripts.functions.parse_all_page_data(page_data)
 
 if __name__ == "__main__":
     main()
