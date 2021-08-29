4b3f03ce5660a1764514c1765cd6816c93a23da4
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-08-29 05:18:26 +0100

    Just some changes

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ def main():
     
     #page_data = scripts.functions.request_all_pages()
     
-    #Saving all page data to a json file
-    
     #scripts.functions.save_as_json(page_data, "data.json")
 
 
@@ -18,6 +16,13 @@ def main():
     scripts.functions.parse_all_page_data(page_data)
 
     scripts.functions.save_as_json(scripts.functions.data_set, "formatted_data.json")
+
+    
+
+    for page in scripts.functions.data_set:
+        scripts.functions.get_two_lowest_values(page)
+        
+        
 
 if __name__ == "__main__":
     main()
