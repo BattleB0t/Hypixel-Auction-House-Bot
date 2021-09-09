e9769f3f35950cb2027343eac61431ee3018bcbe
Henry Dewsnap <74488894+HenDewsnap@users.noreply.github.com>
2021-09-09 22:33:51 +0100

    Update api.py

--- scripts/api.py
+++ scripts/api.py
@@ -1,20 +1,22 @@
 #This script contains all the api request code.
+from typing import final
 import requests
 import threading
 
+final_page_Value = 0
+all_pages_downloaded = False
 
 def request(url):
-    print(f"downloading: {url}")
-    return requests.get(url).json()
+    print(f"Requesting: {url}")
+    return requests.get(url, timeout=10).json()
 
-def request_and_page(url, page_incrimentor):
-    responses[f"page{page_incrimentor}"] = request(url)
 
 def many_requests(urls): #URLs must be a list.
     global responses
-    responses = {}    
+    Final_Page_Value = len(urls)-1
+    responses = {}
     page_incrimentor = 0
     for url in urls:
-        threading.Thread(target=request_and_page, args=(url, page_incrimentor)).start()
+        responses[f"page{page_incrimentor}"] = request(url)
         page_incrimentor += 1
     return responses
